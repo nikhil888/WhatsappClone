eb4c7523bc333d7c247f80437e2856cf7b108f54
unknown <nikkhil888@gmail.com>
2021-03-27 21:15:04 +0530

    fetch contact from api

--- screens/ContactScreen.tsx
+++ screens/ContactScreen.tsx
@@ -1,11 +1,27 @@
-import * as React from 'react';
+import React,{useEffect,useState} from 'react';
 import {FlatList, StyleSheet} from 'react-native';
 import { View } from '../components/Themed';
 import ContactListItem from '../components/ContactListItem';
-import users from '../data/Users';
-
+import {API,graphqlOperation} from "aws-amplify";
+import { listUsers } from "../src/graphql/queries";
 
 export default function ContactScreen() {
+  const [users,setUsers] = useState([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+     try{
+        const userData = await API.graphql(
+          graphqlOperation(
+            listUsers
+          )
+        )
+        setUsers(userData.data.listUsers.items)
+     }catch(e){
+
+     }
+    }
+    fetchUsers();
+  },[])
   return (
     <View style={styles.container}>
       <FlatList

41ebd202d84e7e0feb68807dbf34374be5d64b07
unknown <nikkhil888@gmail.com>
2021-03-27 10:53:34 +0530

    Chat Screen

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,11 +1,17 @@
 import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { ColorSchemeName } from 'react-native';
-
+import { ColorSchemeName,View } from 'react-native';
+import Colors  from '../constants/Colors';
+import {
+  Octicons,
+  MaterialCommunityIcons,
+  MaterialIcons,
+  FontAwesome5,
+} from '@expo/vector-icons';
 import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
-import BottomTabNavigator from './BottomTabNavigator';
+import MainTabNavigator from './MainTabNavigator';
 import LinkingConfiguration from './LinkingConfiguration';
 
 // If you are not familiar with React Navigation, we recommend going through the
@@ -24,12 +30,35 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 function RootNavigator() {
   return (
-    <Stack.Navigator screenOptions={{
-      headerStyle: {
-        backgroundColor:'red',
-      } 
-       }}>
-      <Stack.Screen name="Root" component={BottomTabNavigator} />
+      <Stack.Navigator screenOptions={{
+        headerStyle: {
+          backgroundColor: Colors.light.tint,
+          elevation: 0,
+        },
+        headerTintColor: Colors.light.background,
+        headerTitleAlign: 'left',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        }
+      }}>
+       <Stack.Screen
+        name="Root"
+        component={MainTabNavigator}
+        options={{
+          title: "WhatsApp",
+          headerRight: () => (
+            <View style={{
+              flexDirection: 'row',
+              width: 60,
+              justifyContent: 'space-between',
+              marginRight: 10,
+            }}>
+              <Octicons name="search" size={22} color={'white'} />
+              <MaterialCommunityIcons name="dots-vertical" size={22} color={'white'} />
+            </View>
+          )
+        }}
+      />
       <Stack.Screen name="NotFound" component={NotFoundScreen} options={{ title: 'Oops!' }} />
     </Stack.Navigator>
   );
--- types.tsx
+++ types.tsx
@@ -3,9 +3,11 @@ export type RootStackParamList = {
   NotFound: undefined;
 };
 
-export type BottomTabParamList = {
-  TabOne: undefined;
-  TabTwo: undefined;
+export type MainTabParamList = {
+  Camera: undefined;
+  Chats: undefined;
+  Status: undefined;
+  Calls: undefined;
 };
 
 export type TabOneParamList = {
@@ -15,3 +17,21 @@ export type TabOneParamList = {
 export type TabTwoParamList = {
   TabTwoScreen: undefined;
 };
+
+export type User = {
+  id: String;
+  name: String;
+  imageUri: String;
+}
+
+export type Message = {
+  id: String;
+  content: string;
+  createdAt: string;
+}
+
+export type ChatRoom = {
+  id: String;
+  users: [User];
+  lastMessage: Message;
+}
